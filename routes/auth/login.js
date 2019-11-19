bb408444704a015a9f3f68bd1c03caa41c53d208
Eiosx <eiosxenzo@gmail.com>
2019-11-19 18:10:21 -0500

    changed port variable.

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -4,22 +4,12 @@ const uuid = require("uuid/v4");
 const router = express.Router();
 
 
-//Get 
-router.get("/:id", (req, res) => {
-  try {
-    res.status(200).json("hello there!")
-  } catch(error){
-    res.status(500).json({
-      message: "error getting user",
-      error: error
-    })
-  }
-})
-
 
 //Creat new user Endpoint
 router.post("/", (req, res) => {
   const { first_name, last_name, username, password, email, authCode} = req.body;
+
+  
 
   try {
     res.status(200).json("hello there!")
