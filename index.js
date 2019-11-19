17e16bc4e44c4caf04cc9bc3d98f8ffdb1a8f782
Eiosx <eiosxenzo@gmail.com>
2019-11-19 16:34:04 -0500

    Added dynamic port functionality for heroku app.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const PORT = 5000;
 const server = require('./server');
 
-server.listen(PORT, () => {
+server.listen(process.env.PORT || PORT, () => {
   console.log(`Server is up on port:${PORT}`);
 });
